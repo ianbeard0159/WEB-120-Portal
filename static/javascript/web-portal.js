db8e39aafc5d1250521c4301c3e78e2e731725d6
Ian Beard <bluelink48@yahoo.com>
2018-06-27 16:19:03 -0700

    Worked on menu navigation

--- static/javascript/web-portal.js
+++ static/javascript/web-portal.js
@@ -10,8 +10,24 @@ $(document).ready(function(){
     console.log("Script Running");
     // Initialize Scroll Positions
     scroll_pic("#profile-picture", 35);
+    
+    // Hide menus
+    $("#drop-menu").hide();
+    $(".modal").hide();
 
     window.onscroll = function() {
         scroll_pic("#profile-picture", 35);
     };
+
+    //toggle menus
+    $("#nav-symbol").click(function(){
+        $("#drop-menu").toggle();
+    });
+    $("body").on("click", "#contact",(function(){
+        $("#drop-menu").hide();
+        $(".modal").show();
+    }));
+    $(".modal").on("click", "#cancelBtn",(function(){
+        $(".modal").hide();
+    }));
 });
